067822d5ed2490b893fceb99ff486f1249089f71
ExecutiveOfficer <tech.head.b@gmail.com>
2019-07-09 11:47:55 -0400

    theming pls

--- app/src/main/java/com/example/tucapp/SettingsActivity.java
+++ app/src/main/java/com/example/tucapp/SettingsActivity.java
@@ -4,8 +4,12 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
+
+import java.util.Objects;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -21,12 +25,26 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+//            Preference.OnPreferenceChangeListener opcl = new Preference.OnPreferenceChangeListener() {
+//                @Override
+//                public boolean onPreferenceChange(Preference preference, Object newValue) {
+//                    if(PreferenceManager.getDefaultSharedPreferences(Objects.requireNonNull(getContext())).getBoolean("dark_mode", false)){
+//                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//                        return true;
+//                    } else {
+//                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//                    }
+//                    return false;
+//                }
+//            };
+//            Objects.requireNonNull(findPreference("dark_mode")).setOnPreferenceChangeListener(opcl);
         }
     }
 
